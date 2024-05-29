38c852c6e2a67aac94d3cf223a6689b23395b18a
Seccmu Ivanov <rabbltman@outlook.com>
2024-05-29 17:05:02 +0800

    feat: add scripts fetch file stream info

--- Compressor.py
+++ Compressor.py
@@ -1,19 +1,35 @@
 import os
+import subprocess
+
+from MediaFileInfo import MediaFileInfo
 
 
 class Compressor:
-    def __init__(self, input_video_path: str, output_video_dir: str):
+    def __init__(self):
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.ffmpeg_path = os.path.join(self.current_dir, 'bin', 'ffmpeg.exe')
+        self.output_video_codec = ""
+        self.output_audio_codec = ""
+        self.output_video_bitrate = 500_000
+
+    def run(self, input_video_path, output_video_dir):
         """
         :param input_video_path: original video file
         :param output_video_dir: directory for compressed video output
         """
-        self.input_video_path = input_video_path
-        filename, _ = os.path.splitext(os.path.basename(self.input_video_path))
-        self.output_video_path = output_video_dir + f"{filename}"
-
-    def run(self):
-        pass
+        filename, _ = os.path.splitext(os.path.basename(input_video_path))
+        output_video_path = output_video_dir + f"{filename}_.mp4"
+        for stream in MediaFileInfo(input_video_path).streams:
+            if stream["codec_type"] == "video":
+                pass
+            elif stream["codec_type"] == "audio":
+                pass
+        res = subprocess.run(
+            args=[self.ffmpeg_path, '-i', input_video_path],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True)
 
 
 if __name__ == "__main__":
-    Compressor("example.mp4", "./")
+    Compressor()
---
+++ MediaFileInfo.py
@@ -0,0 +1,55 @@
+import os
+import subprocess
+
+
+class MediaFileInfo:
+    """
+    A class retrieves information from the input media file using ffprobe.
+    """
+
+    def __init__(self, input_file_path: str):
+        self.S_STREAM_KEYWORDS = (
+            "codec_name",
+            "codec_type",
+        )
+        self.F_STREAM_KEYWORDS = (
+            "width",
+            "height",
+            "bit_rate",
+            "r_frame_rate",
+            "sample_rate"
+        )
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.ffprobe_path = os.path.join(self.current_dir, 'bin', 'ffprobe.exe')
+        self.input_path = input_file_path
+        self.streams = self.get_stream_info()
+
+    def get_stream_info(self):
+        res = subprocess.run(
+            args=[self.ffprobe_path, '-i', self.input_path, "-show_streams"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True)
+        streams = []
+        stream_info = res.stdout.split('\n')
+        assert (stream_info[0] == "[STREAM]" and stream_info[-2] == "[/STREAM]")
+        for line in stream_info:
+            if "[STREAM]" in line:
+                streams.append(dict())
+                continue
+            if "=" in line:
+                item, val = line.split("=")
+                if item in self.S_STREAM_KEYWORDS:
+                    streams[-1][item] = val
+                elif item in self.F_STREAM_KEYWORDS:
+                    if "/" in val:
+                        val = [float(f) for f in val.split("/")]
+                        assert (len(val) == 2)
+                        if val[1] == 0:
+                            continue
+                        else:
+                            val = val[0] / val[1]
+                    else:
+                        val = float(val)
+                    streams[-1][item] = val
+        return streams
